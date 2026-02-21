b3ba98c9c1097dd8cca5416c0d06564eff683fa0
xrasdas <gitret76@gmail.com>
2026-02-21 18:48:41 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,19 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: '/', 
+  plugins: [
+    react(), 
+    tailwindcss(), 
+    viteSingleFile()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: 'dist',
+  }
 });
